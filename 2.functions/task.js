572c2a8667bd4d393d729747b253f488f7db9c20
Anton Kudryashov <driver_90@list.ru>
2025-02-15 20:45:52 +0700

    Второе ДЗ

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,91 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  let min = arr[0]
+  let max = 0
+  let avg = 0
+  if (arr.length === 0){
+    return 0
+  }
+  else {
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i] < min){
+        min = arr[i]
+      }
+      else if (arr[i] > max){
+        max = arr[i]
+      }
+      avg += Number((arr[i]/arr.length).toFixed(2))
+    }
+    return { min: min, max: max, avg: avg }
+  }
 }
 
-function summElementsWorker(...arr) {
 
+function summElementsWorker(...arr) {
+  let summ = 0
+  if (arr.length === 0){
+    return 0
+  }
+  else {
+    for (let i =0; i < arr.length; i++){
+      summ += arr[i]
+    }
+    return summ
+  }
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length === 0){
+    return 0
+  }
+  else {
+    let different = Math.max.apply(0, arr)-Math.min.apply(0, arr)
+  return different
+  }
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  let sumEvenElement = 0
+  let sumOddElement = 0
+  if (arr.length === 0){
+    return 0
+  }
+  else{
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i]%2 === 0){
+        sumEvenElement += arr[i]
+      }
+      else{
+        sumOddElement += arr[i]
+      }
+    }
+    return sumEvenElement-sumOddElement
+  }
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  let sumEvenElement = 0
+  let countEvenElement = 0
+  if (arr.length === 0){
+    return 0
+  }
+  else{
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i]%2 === 0){
+        sumEvenElement += arr[i]
+        countEvenElement += 1
+      }
+    } 
+    return sumEvenElement/countEvenElement
+  }
 }
 
 function makeWork (arrOfArr, func) {
-
+  let maxWorkerResult = arrOfArr[0][0]
+  for (let i = 0; i < arrOfArr.length; i++){
+      let result = func(...arrOfArr[i])
+      if (result > maxWorkerResult){
+          maxWorkerResult = result
+      }
+    }
+    return maxWorkerResult
 }
