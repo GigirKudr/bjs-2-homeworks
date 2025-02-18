ecba009b8ffa729a84e53ae05c186f13dc675574
Anton Kudryashov <driver_90@list.ru>
2025-02-18 21:24:02 +0700

    Третье ДЗ

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+    return arr1.every((element, index) => element === arr2[index]);
+  }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+  function getUsersNamesInAgeRange(users, gender) {
+    const userFiltered = users.filter(user => user.gender === gender)
+    if (userFiltered.length === 0) {
+      return 0;
+  }
+    const userMap = userFiltered.map(user => user.age)
+    const userAge = userMap.reduce((sum, age) => sum + age, 0) / userMap.length
+
+    return userAge
 }
